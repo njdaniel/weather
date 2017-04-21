4c21360774f8548a6e55a55fc7d9f6dd3a744ba4
njdaniel <nicholasjdaniel@gmail.com>
2017-04-21 14:12:05 -0400

    add prevent clear on submit

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -17,9 +17,15 @@ export default class SearchBar extends Component {
         this.setState({term: event.target.value})
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // TODO: Fetch weather Data
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input
                     placeholder="Get a five-day forecast in your favorite cities"
                     className="form-control"
