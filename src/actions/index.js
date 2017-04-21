7ca4437956b8fef4d91db1afbaaf9925a635709b
njdaniel <nicholasjdaniel@gmail.com>
2017-04-21 15:34:20 -0400

    add axios ajax call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
 /**
  * Created by nicholas on 4/21/17.
  */
+import axios from 'axios';
+
 const API_KEY = '47df23be39eed91149aca806b747e301';
 
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+      type: FETCH_WEATHER,
+      payload: request
+    };
+}
