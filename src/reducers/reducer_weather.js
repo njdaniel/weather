509ed24a88562a0ea5e8568ad08e84cfa48a1b99
njdaniel <nicholasjdaniel@gmail.com>
2017-04-28 09:52:39 -0400

    add reducer for fetch weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,15 @@
 /**
  * Created by nicholas on 4/21/17.
  */
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // Add to end
+            // return state.concat([action.payload.data]);
+            // Add to beginning
+            return [ action.payload.data, ...state]; // [city, city, city]
+    }
     return state;
 }
