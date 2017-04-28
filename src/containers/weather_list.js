2b57d7434c3e8a213bf651789444744b4c7ac71f
njdaniel <nicholasjdaniel@gmail.com>
2017-04-28 10:08:55 -0400

    add weather list table

---
+++ src/containers/weather_list.js
@@ -0,0 +1,21 @@
+/**
+ * Created by nicholas on 4/28/17.
+ */
+import React, {Component} from 'react';
+
+export default class WeatherList extends Component {
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        )
+    }
+}
