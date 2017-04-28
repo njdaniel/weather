f637575c44a2e2c5b2e95f47afb411935e1d4317
njdaniel <nicholasjdaniel@gmail.com>
2017-04-28 14:15:31 -0400

    update data into table

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -5,6 +5,15 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class WeatherList extends Component {
+
+    renderWeather(cityData) {
+        return (
+            <tr>
+                <td>{cityData.city.name}</td>
+            </tr>
+        )
+    }
+
     render() {
         return (
             <table className="table table-hover">
@@ -17,7 +26,7 @@ class WeatherList extends Component {
                     </tr>
                 </thead>
                 <tbody>
-
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         )
