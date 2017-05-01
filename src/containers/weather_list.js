0c2f43897d145e8a00bd154029034187ac1e2727
njdaniel <nicholasjdaniel@gmail.com>
2017-05-01 12:30:44 -0400

    convert to F

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 /**
  * Created by nicholas on 4/28/17.
  */
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
@@ -9,14 +10,14 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5) -459.67 );
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={temps} color="orange" units="F" /></td>
                 <td><Chart data={pressures} color="green" units="hPa" /></td>
                 <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
@@ -29,7 +30,7 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature (K)</th>
+                        <th>Temperature (F)</th>
                         <th>Pressure (hPa)</th>
                         <th>Humidity (%)</th>
                     </tr>
